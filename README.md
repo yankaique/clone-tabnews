fb47e567927616ecacb5338fd8afd74d8e80e7f8
Yan Marinho <yan.contato.dev@gmail.com>
2025-10-17 13:45:32 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para estudo com base no site https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return( <h1>Maria eu te amo mas não quero ir no show do korn ❤️</h1>)
+  return <h1>Maria eu te amo mas não quero ir no show do korn ❤️</h1>;
 }
 
 export default Home;
