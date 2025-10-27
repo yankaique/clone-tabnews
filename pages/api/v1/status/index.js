9519df1099adb763b79a1dd830988809a4107244
Yan Marinho <yan.contato.dev@gmail.com>
2025-10-27 21:53:54 +0000

    finalizando rota `/api/v1/status` e correção de sql injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,28 @@
 import database from "infra/database.js";
 
 async function status (request, response) {
-  const dbResponse = await database.query("SELECT 1 + 1 as sum;");
-  console.log(dbResponse.rows[0]);
+  const databasePostgresVersion = await database.query("SHOW server_version;");
+  const postgresVersion = databasePostgresVersion.rows[0].server_version
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = databaseMaxConnections.rows[0].max_connections
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const openedConnections = databaseOpenedConnections.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    "name": "Oi Yanzito"
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  
+  const responseJson = await response.json();
+  expect(responseJson.updated_at).toBeDefined();
+  expect(responseJson.dependencies.database.version).toBe("16.0");
+  expect(responseJson.dependencies.database.max_connections).toBe(100);
+  expect(responseJson.dependencies.database.opened_connections).toBe(1);
 });
